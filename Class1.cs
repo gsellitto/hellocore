5493633855e0cc015dfd99d9d8d04975611413f7
gsellitto <gianluigi@sellitto.it>
2018-05-22 14:22:52 +0200

    test

--- Class1.cs
+++ Class1.cs
@@ -6,6 +6,8 @@ public class Class1 {
     public static void  test()
     {
         Console.WriteLine("Hello World! from class test");
+        int pippo=0;
+        Console.WriteLine($"Hello World! from class test {pippo}");
     }
 }
 }
